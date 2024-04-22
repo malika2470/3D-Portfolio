170018fda4429370e42a7ad75306314755323b5a
malika2470 <malika2470@gmail.com>
2024-04-22 12:21:18 -0400

    Basic setup

---
+++ src/assets/index.js
@@ -0,0 +1,27 @@
+// Importing technology-related images from the assets/tech directory
+import cImage from "./assets/tech/c.png";
+import cppImage from "./assets/tech/c++.png";
+import cssImage from "./assets/tech/css.png";
+import htmlImage from "./assets/tech/html.png";
+import javaImage from "./assets/tech/java.png";
+import javascriptImage from "./assets/tech/javascript.png";
+import pythonImage from "./assets/tech/python.png";
+import reactImage from "./assets/tech/react.png";
+
+// Importing other images from the assets directory
+import logo from "./assets/logo.png";
+import reactLogo from "./assets/react.svg";
+
+// Exporting all imported images
+export {
+  cImage,
+  cppImage,
+  cssImage,
+  htmlImage,
+  javaImage,
+  javascriptImage,
+  pythonImage,
+  reactImage,
+  logo,
+  reactLogo
+};
